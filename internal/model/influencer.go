98e5e5d5c5560231745df26346392ef0760ab611
dokshiye <okshieva@gmail.com>
2024-04-27 03:43:39 +0500

    feat: add model for influencer info

---
+++ internal/model/influencer.go
@@ -0,0 +1,13 @@
+package model
+
+type InfluencerInfo struct {
+	Rank        string
+	Account     string
+	Name        string
+	AvatarImage string
+	Category    []string
+	Subscribers string
+	Country     string
+	EngAuth     string
+	EngAvg      string
+}
